25a81552112e5af60382763123fe7e7f5be41746
borntofrappe <borntofrappe@gmail.com>
2019-09-26 23:43:59 +0200

    update project description and set up the svelte environment

---
+++ Sport Bracket/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	// components behind the application
+	import Header from './Header.svelte';
+
+</script>
+
+<style>
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	:global(body > * + *) {
+		margin-top: 1rem;
+	}
+</style>
+
+<Header title="Who's going to win?"/>
+
---
+++ Sport Bracket/src/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let title = "Sport bracket";
+</script>
+<header>
+    <h1>
+        {title}
+    </h1>
+</header>
