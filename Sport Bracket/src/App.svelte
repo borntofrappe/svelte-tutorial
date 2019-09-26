ba1bc94bbcd571b00e543a28aad6006fddf7254a
borntofrappe <borntofrappe@gmail.com>
2019-09-26 23:51:12 +0200

    style the brackets to show the matches in a row

--- Sport Bracket/src/App.svelte
+++ Sport Bracket/src/App.svelte
@@ -17,6 +17,30 @@
 
 </script>
 
+<style>
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	:global(body > * + *) {
+		margin-top: 1rem;
+	}
+
+	.bracket {
+		max-width: 1000px;
+		width: 100%;
+		display: flex;
+	}
+</style>
 
 <Header title="Who's going to win?"/>
-<Bracket {teams} />
+
+<main class="bracket">
+	<Bracket {teams} />
+</main>
--- Sport Bracket/src/Bracket.svelte
+++ Sport Bracket/src/Bracket.svelte
@@ -9,6 +9,60 @@
     $: winner = teams.length === 1 && teams[0];
 </script>
 
+<style>
+    /* to align the names vertically, stretch the list items in a flex-column  */
+    ul {
+        display: flex;
+        flex-direction: column;
+        /* flex grow to have the lists stretch and cover the available width (same as the heading) */
+        flex-grow: 1;
+        list-style: none;
+    }
+    ul li,
+    h2 {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 1rem 0.5rem;
+        text-align: center;
+    }
+    ul li {
+        position: relative;
+    }
+    /* with pseudo elements draw the following structure next to each couplet
+    ---
+        |
+        |
+    ---
+
+    this by drawing half the structure on the even items and the mirrored tructure on the odd ones
+    */
+    ul li:before,
+    ul li:after {
+        position: absolute;
+        content: "";
+        top: 50%;
+        left: 100%;
+        background: currentColor;
+    }
+    ul li:before {
+        width: 1rem;
+        height: 2px;
+        transform: translate(-100%, -50%);
+    }
+    ul li:after {
+        width: 2px;
+        height: 100%;
+    }
+    ul:nth-of-type(n) li:nth-of-type(odd):after {
+        transform: translate(0%, 0%);
+    }
+    ul:nth-of-type(n) li:nth-of-type(even):after {
+        transform: translate(0%, -100%);
+    }
+</style>
+
 <!-- display the winner in a heading or an unordered list with the teams if no winner is identified -->
 {#if winner}
 <h2>{winner}</h2>
