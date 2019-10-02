8d70acd83c15b6b2c9424b812f2ff353446df8a6
borntofrappe <borntofrappe@gmail.com>
2019-10-03 00:08:13 +0200

    include a single purpose project explaining readable stores

--- Stopwatch/README.md
+++ Stopwatch/README.md
@@ -13,3 +13,5 @@ Create an application allowing to track the passing of time and the lapse betwee
 -   the goal is to create a pull request to a popular repository, showcasing a functioning stopwatch app.
 
 -   in the **res** folder you find the design of the application, broken up in two smaller projects. **Stopwatch SVG** describes the SVG and its eventual animation, while **Stopwatch UI** describes the layout and styles for the overall application.
+
+-   at the [following url](https://svelte.dev/repl/4e47e8ba30a8499faaaef85c452804a1) you find a basic idea as to how the passing of time is retrieved from a readable store. Given the intricacies behind the readable function, and the fact that the project needs to track / stop tracking the value of the store more than once, I decided to devote an entire project to the start/pause/stop feature. I included the main files `App.svelte` and `stores.js` in the **res** folder as well, to provide a local frame of reference.
---
+++ Stopwatch/res/Readable Store/App.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { time } from "./stores.js";
+
+    // previous is set to record the time accumulated before the pause button is pressed
+    let previous = 0;
+    // lapse is set to consider the time since the start button is first pressed
+    let lapse = 0;
+
+    // unsubscribe is set to refer to the function used to unsubscribe from the store
+    let unsubscribe;
+
+    // through the start function pair the lapse variable to the time retrieved from the readable store
+    function start() {
+        // assign the stop function to unsubscribe
+        unsubscribe = time.subscribe(value => {
+            // add the previous value
+            lapse = value + previous;
+        });
+    }
+
+    // through the terminate function unsubscribe from the readable store
+    function terminate() {
+        // check if unsubscribe is truthy (this to cover the situation in which the stop button is pressed after the pause button)
+        if (unsubscribe) {
+            unsubscribe();
+            unsubscribe = null;
+        }
+    }
+
+    // through the stop function unsubscribe from the readable store and reset the values
+    function stop() {
+        terminate();
+        lapse = 0;
+        previous = 0;
+    }
+
+    // through the pause function unsubscribe from the store and set previous to match the value held by lapse
+    function pause() {
+        terminate();
+        previous = lapse;
+    }
+</script>
+
+<!-- by using the value of unsubscribe and lapse disable the different buttons -->
+<button disabled="{unsubscribe}" on:click="{start}">
+    Start
+</button>
+<button disabled="{!unsubscribe}" on:click="{pause}">
+    Pause
+</button>
+<button disabled="{!unsubscribe && !lapse}" on:click="{stop}">
+    Stop
+</button>
+
+<h1>{lapse ? lapse : '0'}</h1>
