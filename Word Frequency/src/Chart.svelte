8b1b74cd2c3ad6e02484d16edc74e8e4f7c55800
borntofrappe <borntofrappe@gmail.com>
2019-10-05 14:13:09 +0200

    animate the visualization and limit the number of slices

--- Word Frequency/src/Chart.svelte
+++ Word Frequency/src/Chart.svelte
@@ -1,13 +1,33 @@
 <script>
-    // map the data to a lollipop chart
+    // transition to fade the visualization after the table
+    import { fly } from "svelte/transition";
+    // instead of mapping the entire dataset consider only the most 5 frequent words and add a sixth category for "other" words
     export let data;
+    const cap = 5;
+
+    $: subset =
+        data.length > cap
+            ? [
+                  ...data.slice(0, cap),
+                  data.slice(cap).reduce(
+                      (acc, curr) => ({
+                          word: "...",
+                          frequency: acc.frequency ? acc.frequency + curr.frequency : curr.frequency
+                      }),
+                      {}
+                  )
+              ]
+            : data;
 
     // values for the visualization
     const width = 100;
     const height = 100;
 
     // slice the pie according to the individual frequencies
-    const pie = d3.pie().value(d => d.frequency);
+    const pie = d3
+        .pie()
+        .value(d => d.frequency)
+        .sort(null);
 
     // arc used for the slices
     // use an outerRadius smaller than half the width to allocate space for the text
@@ -24,7 +44,7 @@
         .outerRadius(width / 2.25);
 
     // color scale for the slices
-    const color = d3.scaleOrdinal(d3.schemePastel1);
+    const color = d3.scaleOrdinal(d3.schemeDark2);
 </script>
 
 <style>
@@ -40,7 +60,7 @@ have the svg expand to cover the available width and height */
     }
 </style>
 
-<section class="chart">
+<section class="chart" transition:fly="{{ x: -25, delay: 25 }}">
     <svg class="icon" viewBox="0 0 100 100" width="50" height="50">
         <use href="#chart"></use>
     </svg>
@@ -53,15 +73,15 @@ have the svg expand to cover the available width and height */
         <!-- center the arcs in the svg -->
         <g transform="translate({width / 2} {height / 2})">
             <!-- for each datum add an arc using the pie-d data -->
-            {#each pie(data) as datum, i (datum.data.word)}
+            {#each pie(subset) as datum, i (datum.data.word)}
             <!-- between the text and the slice include a stroke connecting the two centroids
             included before the slice to have the line beneath the path element
             -->
-            <path clip-path="url(#clip)" fill="none" stroke="currentColor" stroke-width="0.5" d="M {arc.centroid(datum).join(' ')} L {arcText.centroid(datum).join(' ')}">{datum.data.word}</path>
+            <path clip-path="url(#clip)" fill="none" stroke="{color(i)}" stroke-width="0.5" d="M {arc.centroid(datum).join(' ')} L {arcText.centroid(datum).join(' ')}">{datum.data.word}</path>
             <!-- slice -->
             <path d="{arc(datum)}" fill="{color(i)}" stroke="hsl(0, 0%, 100%)" stroke-width="1"></path>
             <!-- next to each slice include a text with the second arc -->
-            <text x="0" y="0" font-size="4" text-anchor="middle" dominant-baseline="middle" transform="translate({arcText.centroid(datum).join(' ')})">{datum.data.word}</text>
+            <text x="0" y="0" font-size="5" text-anchor="middle" dominant-baseline="middle" transform="translate({arcText.centroid(datum).join(' ')})">{datum.data.word}</text>
             {/each}
         </g>
     </svg>
