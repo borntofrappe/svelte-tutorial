33786d4497e2da4505249a619943a4d9c85cf8b0
borntofrappe <borntofrappe@gmail.com>
2019-10-29 23:58:34 +0100

    include the selection component

--- Picross/src/App.svelte
+++ Picross/src/App.svelte
@@ -1,1 +1,42 @@
-<h1>Picross</h1>
+<script>
+    import Selection from './Selection.svelte';
+
+    // variable describing the selection
+    let selection;
+
+    // levels describing the actual content through a string of x(s) and o(s)
+    const levels = [
+        {
+            name: "Star",
+            level: "ooxooooxooxxxxxoxxxooxoxo"
+        },
+        {
+            name: "Mobile phone",
+            level: "xxxoxxoooxxoxoxxoooxxooox"
+        },
+        {
+            name: "Heart",
+            level: "ooxooooooooooooxoooxxxoxx"
+        },
+        {
+            name: "Musical note",
+            level: "xxoooxxoxoxxoxooooxxoooxx"
+        },
+        {
+            name: "Moon",
+            level: "xxooxxxxoooxxoooooooxooox"
+        }
+    ];
+
+    // function called when a selection is made in the selection component
+    function handleSelection(e) {
+        // update the selection with the level matching the name received in through the event
+        selection = levels.find(({ name }) => name === e.detail);
+    }
+
+    $:console.log(selection);
+
+</script>
+
+<!-- pass in the selection component an array describing the levels' names -->
+<Selection on:selection="{handleSelection}" options="{levels.map(({name}) => name)}"/>
---
+++ Picross/src/Selection.svelte
@@ -0,0 +1,29 @@
+<script>
+    // dispatch a selection event when clicking on one of the buttons
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    function select(selection) {
+        dispatch("selection", selection);
+    }
+
+    // include one button for each option
+    export let options = ["Star"];
+</script>
+
+<style>
+    /* display the buttons in a column */
+    section {
+        display: flex;
+        flex-direction: column;
+    }
+    section > * + * {
+        margin-top: 1rem;
+    }
+</style>
+
+<section>
+    {#each options as option, index}
+    <button on:click="{() => select(option)}">{option}</button>
+    {/each}
+</section>
