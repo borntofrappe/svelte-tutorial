b8c8be94b7e4bb216e2a1043ef955c20ba2b6346
borntofrappe <borntofrappe@gmail.com>
2019-11-02 17:34:07 +0100

    restructure the application to reduce the updates to the markdown string

--- Markdown Editor/src/Editor.svelte
+++ Markdown Editor/src/Editor.svelte
@@ -4,8 +4,8 @@
     export let markdown;
     let value = markdown;
 
-    function handleInput() {
-        dispatch("update", value);
+    function handlePreview() {
+        dispatch("preview", value);
     }
 </script>
 <style>
@@ -22,4 +22,55 @@
         outline: 2px solid hsl(100, 90%, 35%);
     }
 </style>
-<textarea bind:value on:input="{handleInput}"></textarea>
+
+
+<nav>
+    <!-- TODO: functionality behind the first three buttons -->
+    <button title="Add code block" aria-label="Add code block">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <g transform="translate(5 5)" stroke-width="10" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" fill="none">
+                <path id="bracket--left" d="M 30 18 l -30 27 30 27" />
+                <use href="#bracket--left" transform="translate(90 0) scale(-1 1)" />
+            </g>
+        </svg>
+    </button>
+    <button title="Add link" aria-label="Add link">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <g transform="translate(5 5)" stroke-width="10" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" fill="none">
+                <path id="curve--left" d="M 35 25 h -10 a 20 20 0 0 0 0 40 h 10" />
+                <use href="#curve--left" transform="translate(90 0) scale(-1 1)" />
+                <path d="M 30 45 h 30" />
+            </g>
+        </svg>
+    </button>
+    <button title="Download markdown" aria-label="Download markdown">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <g transform="translate(5 5)" stroke-width="10" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" fill="none">
+                <path d="M 45 0 v 60 l -22 -22 m 22 22 l 22 -22" />
+                <path d="M 0 60 v 20 a 10 10 0 0 0 10 10 h 70 a 10 10 0 0 0 10 -10 v -20" />
+            </g>
+        </svg>
+    </button>
+
+    <!-- on click dispatch the preview event to show the appropriate component -->
+    <button on:click="{handlePreview}" title="Preview markdown" aria-label="Download markdown">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <defs>
+                <mask id="mask--circle" maskUnits="userSpaceOnUse">
+                    <rect x="0" y="0" width="100" height="100" fill="white" />
+                    <circle cx="50" cy="50" r="24" fill="black" />
+                </mask>
+            </defs>
+            <g mask="url(#mask--circle)">
+                <g transform="translate(5 5)" stroke-width="10" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" fill="currentColor">
+                    <path d="M 0 45 c 25 -40 65 -40 90 0 -25 40 -65 40 -90 0" />
+                </g>
+            </g>
+            <g transform="translate(50 50)">
+                <circle cx="0" cy="0" r="14" fill="currentColor" />
+            </g>
+        </svg>
+    </button>
+</nav>
+
+<textarea bind:value></textarea>
--- Markdown Editor/src/Preview.svelte
+++ Markdown Editor/src/Preview.svelte
@@ -1,5 +1,22 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
     export let markup;
 </script>
+
+<style></style>
+
+<!-- show the markup below the navigation bar -->
+<nav>
+    <!-- on click dispatch the event to show the edit component -->
+    <button on:click="{() => dispatch('edit')}" title="Edit markdown" aria-label="Edit markdown">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+            <g transform="translate(5 5)" stroke-width="10" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" fill="none">
+                <path d="M 73 0 l 17 17 -45 45 -17 0 0 -17 45 -45" />
+                <path d="M 35 10 h -25 a 10 10 0 0 0 -10 10 v 60 a 10 10 0 0 0 10 10 h 60 a 10 10 0 0 0 10 -10 v -25" />
+            </g>
+        </svg>
+    </button>
+</nav>
 
 {@html markup}
