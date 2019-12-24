b31beb2948d2c7ca05951ca8933be9aab700d152
borntofrappe <borntofrappe@gmail.com>
2019-12-25 00:01:49 +0100

    add svg title and update svelte's bg

--- Star Wars Crawl/README.md
+++ Star Wars Crawl/README.md
@@ -8,4 +8,6 @@ Create an interactive form allowing to edit and show the opening crawls from a S
 
 This project begun while developing a toggle out of a light saber, and is deeply inspired by [this article @lemondefr](https://www.lemonde.fr/pixels/article/2019/12/18/star-wars-vous-voulez-davantage-d-episodes-generez-un-scenario-avec-notre-starwarsotron_6023326_4408996.html).
 
-You can find this toggle in the **res** folder.
+You can find the toggle in the **res** folder, using SVG syntax ans CSS transition properties to show the blue-colored saber.
+
+In addition to this project, I also re-designed the "Star Wars" title I first designed for a separate React project. In the **res** folder I added this resource, and you can find its first version [on codesandbox](https://4p7l5.codesandbox.io/).
--- Star Wars Crawl/src/App.svelte
+++ Star Wars Crawl/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Toggle from './Toggle.svelte';
 	import Crawls from './Crawls.svelte';
-	
+
 	let showCrawls = false;
 	let title = 'The Phantom Menace';
 	let text = 'Turmoil has engulfed the Galactic Republic. The taxation of trade routes to outlying star systems is in dispute. Hoping to resolve the matter with a blockade of deadly battleships, the greedy Trade Federation has stopped all shipping to the small planet of Naboo. While the Congress of the Republic endlessly debates this alarming chain of events, the Supreme Chancellor has secretly dispatched two Jedi Knights, the guardians of peace and justice in the galaxy, to settle the conflict....';
@@ -9,7 +9,9 @@
 
 <style>
 	:global(body) {
-		background: hsl(0, 0%, 0%);
+		background: url("data:image/svg+xml,%3Csvg opacity='0.2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'%3E%3Cdefs%3E%3Ccircle id='circle' r='1' fill='hsl(0, 0%25, 100%25)' /%3E%3C/defs%3E%3Cuse href='%23circle' x='50' y='50' /%3E%3Cuse href='%23circle' x='150' y='50' /%3E%3Cuse href='%23circle' x='50' y='150' /%3E%3Cuse href='%23circle' x='150' y='150' /%3E%3Cuse href='%23circle' transform='translate(100 100) scale(2.5)' /%3E%3C/svg%3E"),
+    hsl(0, 0%, 0%);
+  background-size: 50px, 100%;
 	}
 	div {
 		display: flex;
@@ -35,12 +37,12 @@
 <div>
 	<Toggle on:toggle={(e) => showCrawls = e.detail} />
 	{#if showCrawls}
-	<Crawls {title} {text} />	
+	<Crawls {title} {text} />
 	{:else}
 	<form on:submit|preventDefault>
 		<input type="text" bind:value={title} />
 		<textarea bind:value={text}></textarea>
-	</form>	
+	</form>
 	{/if}
-	
+
 </div>
