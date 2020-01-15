8659b3c1875382109721798cba7eb74e7baf3265
borntofrappe <borntofrappe@gmail.com>
2020-01-16 00:09:27 +0100

    include basic structure for the operations

--- Arithmetic Formatter/src/App.svelte
+++ Arithmetic Formatter/src/App.svelte
@@ -1,1 +1,27 @@
-<h1>Arithmetic Formatter</h1>
+<script>
+  let num1 = 0;
+  let num2 = 0;
+  let operator = "+";
+</script>
+
+<style>
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  p {
+    font-size: 2rem;
+  }
+</style>
+
+<input type="number" bind:value="{num1}" />
+<select bind:value="{operator}">
+  <option value="+">+</option>
+  <option value="-">-</option>
+</select>
+<input type="number" bind:value="{num2}" />
+
+<p>
+  {num1} {operator} {num2} = {operator === '+' ? num1 + num2 : num1 - num2}
+</p>
