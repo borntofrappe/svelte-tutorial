3d35b91f6d2996f3ad4df260e14ce6e34f16700f
borntofrappe <borntofrappe@gmail.com>
2021-04-08 10:54:28 +0200

    code | update - roman clock

--- Roman Clock/README.md
+++ Roman Clock/README.md
@@ -1,17 +1,13 @@
 # [Roman Clock](https://svelte.dev/repl/ce67640a4c034cfa8585f3555fff3f8d)
 
-## Goal
-
-Use a readable store and SVG syntax to display the current time in a classic visual.
-
 ## Notes
 
 The project is perhaps heavier on SVG syntax than Svelte's own logic, but it does showcase a few features of the compiler:
 
--   readable store.
+- readable store
 
--   reactive variables.
+- reactive variables
 
--   the motion package.
+- the motion package
 
-With regards to this last dependency, there's currently a bug when the number of minutes, or hours, switches back to 0. In this instance the rotation occurs from `330` degrees, backwards.
+With regards to this last dependency, there's currently a bug when the number of minutes, or hours, switches back to 0. In this instance the rotation occurs from `360` degrees, backwards.
--- Roman Clock/src/App.svelte
+++ Roman Clock/src/App.svelte
@@ -1,21 +1,21 @@
 <script>
-	import Clock from './Clock.svelte';
-    import { time } from "./stores.js";
-    import { tweened } from "svelte/motion";
-    import { quintInOut } from "svelte/easing";
+  import Clock from './Clock.svelte';
+  import { time } from './stores.js';
+  import { tweened } from 'svelte/motion';
+  import { quintInOut } from 'svelte/easing';
 
-    const options = {
-        easing: quintInOut,
-        duration: 500
-    };
+  const options = {
+    easing: quintInOut,
+    duration: 500,
+  };
 
-	// initialize hours and minutes t refer to the current time
-    let hours = tweened($time.getHours(), options);
-    let minutes = tweened($time.getMinutes(), options);
+  // initialize hours and minutes t refer to the current time
+  let hours = tweened($time.getHours(), options);
+  let minutes = tweened($time.getMinutes(), options);
 
-	// update the variables only as the hours and minutes change
-    $: hours.set($time.getHours());
-    $: minutes.set($time.getMinutes());
+  // update the variables only as the hours and minutes change
+  $: hours.set($time.getHours());
+  $: minutes.set($time.getMinutes());
 </script>
 
 <Clock {hours} {minutes} />
