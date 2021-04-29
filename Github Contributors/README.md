41db3115616e6d92fd15163e9d3f32cbcccad169
borntofrappe <borntofrappe@gmail.com>
2021-04-29 18:04:17 +0200

    code - contributor component

--- Github Contributors/README.md
+++ Github Contributors/README.md
@@ -1,3 +1,13 @@
 # Github Contributors
 
-The goal of this project is to recreate the visualization proposed by Github when highlighting the contributors to a public repository. Consider for instance [this example for sveltejs/kit](https://github.com/sveltejs/kit/graphs/contributors).
+## Notes
+
+The goal of this repository is to replicate one of the visualizations Github proposes in the insight section of public repository, and specifically the one dedicated to contributors.
+
+The visualization is composed of two main areas: a line chart highlighting the number of commits for the entire repository and a series of components detailing the same metric for each and every contributor.
+
+### res
+
+In the `res` folder I dedicate a couple of projects to the building blocks of the application:
+
+- `Contributor` describes the design of the component for individual contributors.
--- Github Contributors/src/App.svelte
+++ Github Contributors/src/App.svelte
@@ -1,3 +1,66 @@
+<script>
+  import Contributor from './Contributor.svelte';
+
+  const data = [
+    {
+      name: 'Rachel',
+      commits: 232,
+      additions: 2100,
+      deletions: 1925,
+    },
+    {
+      name: 'John',
+      commits: 198,
+      additions: 1225,
+      deletions: 1176,
+    },
+    {
+      name: 'Eliza',
+      commits: 14,
+      additions: 178,
+      deletions: 152,
+    },
+    {
+      name: 'Timothy',
+      commits: 8,
+      additions: 98,
+      deletions: 77,
+    },
+  ];
+</script>
+
 <svelte:head>
-	<title>Github Contributors</title>
+  <title>Github Contributors</title>
 </svelte:head>
+
+<svg style="display: none;" viewBox="0 0 100 100">
+  <symbol id="profile-pic">
+    <circle fill="hsl(0, 0%, 92%)" cx="50" cy="50" r="50" />
+    <circle fill="hsl(0, 0%, 72%)" cx="35" cy="38" r="12" />
+    <circle fill="hsl(0, 0%, 72%)" cx="65" cy="38" r="12" />
+    <path
+      stroke="hsl(0, 0%, 72%)"
+      stroke-width="10"
+      fill="none"
+      d="M 38 60 a 12 12 0 0 0 24 0"
+    />
+  </symbol>
+</svg>
+
+<div>
+  {#each data as d, index}
+    <Contributor {...d} index={index + 1} />
+  {/each}
+</div>
+
+<style>
+  div {
+    max-width: 700px;
+    width: 90vw;
+    margin: 1rem auto;
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: repeat(auto-fit, 325px);
+    justify-content: center;
+  }
+</style>
