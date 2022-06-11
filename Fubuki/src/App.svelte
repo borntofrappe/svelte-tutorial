2c8c637ca11df733d877c45f9405432131534378
borntofrappe <borntofrappe@gmail.com>
2022-06-11 15:11:49 +0200

    add feedback visuals
    change the opacity of the rows and columns to highlight when the values match the corresponding totals; change the fill of the text to highlight when the value is repeated and needs to change

--- Fubuki/src/App.svelte
+++ Fubuki/src/App.svelte
@@ -5,31 +5,87 @@
 
   const nums = puzzle.nums.map((row, r) =>
     row.map((d, c) => {
-      const locked = puzzle.locks.includes(r * puzzle.size + c);
-      const n = locked ? d : 0;
+      const n = puzzle.locks.includes(r * puzzle.size + c) ? d : 0;
       return {
         d,
         n,
-        locked,
+        locked: n === d,
       };
     })
   );
 
-  $: rows = nums.reduce(
-    (acc, curr) => [...acc, curr.reduce((a, c) => a + c.n, 0)],
-    []
-  );
+  let rows = [];
+  let columns = [];
+  let issues = [];
+  let solved;
 
-  $: columns = Array(nums.length)
-    .fill()
-    .map((_, i) =>
-      Array(nums[i].length)
-        .fill()
-        .map((_, j) => nums[j][i])
-        .reduce((acc, curr) => acc + curr.n, 0)
+  $: if (nums) {
+    rows = nums.reduce(
+      (acc, curr) => [...acc, curr.reduce((a, c) => a + c.n, 0)],
+      []
     );
 
+    columns = Array(nums.length)
+      .fill()
+      .map((_, i) =>
+        Array(nums[i].length)
+          .fill()
+          .map((_, j) => nums[j][i])
+          .reduce((acc, curr) => acc + curr.n, 0)
+      );
+
+    const values = nums.reduce((acc, curr) => [...acc, ...curr], []);
+    issues = values.reduce(
+      (acc, curr, i) =>
+        !curr.locked &&
+        [...values.slice(0, i), ...values.slice(i + 1)].find(
+          (d) => d.n && d.n === curr.n
+        )
+          ? [...acc, i]
+          : [...acc],
+      []
+    );
+
+    let totalsMatch = true;
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i] !== puzzle.rows[i]) {
+        totalsMatch = false;
+        break;
+      }
+    }
+    if (totalsMatch) {
+      for (let i = 0; i < rows.length; i++) {
+        if (columns[i] !== puzzle.columns[i]) {
+          totalsMatch = false;
+          break;
+        }
+      }
+    }
+
+    if (totalsMatch) {
+      solved = puzzle.nums
+        .reduce(
+          (acc, curr) => [...acc, ...curr.reduce((a, c) => [...a, c], [])],
+          []
+        )
+        .every((d) =>
+          nums
+            .reduce(
+              (acc, curr) => [
+                ...acc,
+                ...curr.reduce((a, c) => [...a, c.n], []),
+              ],
+              []
+            )
+            .includes(d)
+        );
+    }
+  }
+
   let focus;
+
+  $: if (solved) focus = null;
+
   const buttons = nums
     .reduce(
       (acc, curr) => [
@@ -38,7 +94,7 @@
       ],
       []
     )
-    .sort();
+    .sort((a, b) => a - b);
 
   const handleFocus = ({ c, r }) => {
     focus = { c, r };
@@ -62,14 +118,6 @@
 
     nums[focus.r][focus.c].n = n;
   };
-
-  $: solved = nums
-    .reduce((acc, curr) => [...acc, ...curr], [])
-    .every(({ d, n }) => d === n);
-
-  $: if (solved) {
-    focus = null;
-  }
 </script>
 
 <svelte:body on:keydown={handleKey} on:click={handleBlur} />
@@ -77,164 +125,144 @@
 <article>
   <svg viewBox="0 0 {puzzle.size + 2} {puzzle.size + 2}">
     <g transform="translate(0.5 0.5)">
-      <g transform="translate(1 {puzzle.size + 1})">
-        {#each puzzle.columns as c, i}
-          <g transform="translate({i} 0)">
-            <circle
-              r="0.35"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="0.02"
-            />
-            <text
-              font-family="monospace"
-              font-size="0.3"
-              text-anchor="middle"
-              dominant-baseline="middle"
-              fill="currentColor">{c}</text
-            >
-          </g>
-        {/each}
-      </g>
-
-      <g transform="translate({puzzle.size + 1} 1)">
-        {#each puzzle.rows as r, i}
-          <g transform="translate(0 {i})">
-            <circle
-              r="0.35"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="0.02"
-            />
-            <text
-              font-family="monospace"
-              font-size="0.3"
-              text-anchor="middle"
-              dominant-baseline="middle"
-              fill="currentColor">{r}</text
-            >
-          </g>
-        {/each}
-      </g>
-
-      <g opacity="0.6">
-        <g transform="translate(1 0)">
-          {#each columns as c, i}
-            <g transform="translate({i} 0)">
-              <path
-                opacity="0.25"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="0.05"
-                d="M 0 1 v {rows.length - 1}"
-              />
-              <circle
-                r="0.35"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="0.02"
-              />
-              <text
-                font-family="monospace"
-                font-size="0.3"
-                text-anchor="middle"
-                dominant-baseline="middle"
-                fill="currentColor">{c}</text
-              >
-            </g>
-          {/each}
-        </g>
-
-        <g transform="translate(0 1)">
-          {#each rows as r, i}
-            <g transform="translate(0 {i})">
-              <path
-                opacity="0.25"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="0.05"
-                d="M 1 0 h {columns.length - 1}"
-              />
-              <circle
-                r="0.35"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="0.02"
-              />
-              <text
-                font-family="monospace"
-                font-size="0.3"
-                text-anchor="middle"
-                dominant-baseline="middle"
-                fill="currentColor">{r}</text
-              >
-            </g>
-          {/each}
-        </g>
-      </g>
-
-      <g transform="translate(1 1)">
-        {#if focus}
-          <g transform="translate({focus.c} {focus.r})">
-            <rect
-              fill="none"
-              stroke="#fcb22d"
-              stroke-width="0.2"
-              x="-0.38"
-              y="-0.38"
-              width="0.76"
-              height="0.76"
-              rx="0.15"
-            />
-          </g>
-        {/if}
-
-        {#each nums as row, i}
-          <g transform="translate(0 {i})">
-            {#each row as { n, locked }, j}
-              <g transform="translate({j} 0)">
-                <g
-                  class:solved
-                  style="animation-delay: {(i + j) % 2 ? 0 : 0.1}s"
-                  opacity="0"
-                >
-                  <rect
-                    fill="none"
-                    stroke="#fcb22d"
-                    stroke-width="0.2"
-                    x="-0.38"
-                    y="-0.38"
-                    width="0.76"
-                    height="0.76"
-                    rx="0.15"
-                  />
-                </g>
-
-                <rect
-                  fill={locked ? "#017297" : "#02a5dc"}
-                  x="-0.38"
-                  y="-0.38"
-                  width="0.76"
-                  height="0.76"
-                  rx="0.15"
+      <g>
+        <g>
+          <g transform="translate(1 {puzzle.size + 1})">
+            {#each puzzle.columns as c, i}
+              <g transform="translate({i} 0)">
+                <circle
+                  r="0.35"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="0.02"
                 />
-                {#if n !== 0}
+                <text
+                  font-family="monospace"
+                  font-size="0.3"
+                  text-anchor="middle"
+                  dominant-baseline="middle"
+                  y="0.01"
+                  fill="currentColor">{c}</text
+                >
+              </g>
+            {/each}
+          </g>
+
+          <g transform="translate({puzzle.size + 1} 1)">
+            {#each puzzle.rows as r, i}
+              <g transform="translate(0 {i})">
+                <circle
+                  r="0.35"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="0.02"
+                />
+                <text
+                  font-family="monospace"
+                  font-size="0.3"
+                  text-anchor="middle"
+                  dominant-baseline="middle"
+                  y="0.01"
+                  fill="currentColor">{r}</text
+                >
+              </g>
+            {/each}
+          </g>
+        </g>
+
+        <g>
+          <g transform="translate(1 0)">
+            {#each columns as c, i}
+              <g opacity={c === puzzle.columns[i] ? 1 : 0.55}>
+                <g transform="translate({i} 0)">
+                  <path
+                    opacity="0.2"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="0.05"
+                    d="M 0 1 v {rows.length - 1}"
+                  />
+                  <circle
+                    r="0.35"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="0.02"
+                  />
                   <text
+                    font-family="monospace"
+                    font-size="0.3"
                     text-anchor="middle"
                     dominant-baseline="middle"
-                    y="0.03"
-                    font-size="0.4"
-                    font-family="monospace"
-                    fill="hsl(0, 0%, 97%)">{n}</text
+                    y="0.01"
+                    fill="currentColor">{c}</text
                   >
-                {/if}
-                {#if !locked && !solved}
+                </g>
+              </g>
+            {/each}
+          </g>
+
+          <g transform="translate(0 1)">
+            {#each rows as r, i}
+              <g opacity={r === puzzle.rows[i] ? 1 : 0.55}>
+                <g transform="translate(0 {i})">
+                  <path
+                    opacity="0.2"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="0.05"
+                    d="M 1 0 h {columns.length - 1}"
+                  />
+                  <circle
+                    r="0.35"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="0.02"
+                  />
+                  <text
+                    font-family="monospace"
+                    font-size="0.3"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    y="0.01"
+                    fill="currentColor">{r}</text
+                  >
+                </g>
+              </g>
+            {/each}
+          </g>
+        </g>
+      </g>
+
+      <g>
+        <g transform="translate(1 1)">
+          {#if focus}
+            <g transform="translate({focus.c} {focus.r})">
+              <rect
+                fill="none"
+                stroke="#e98e31"
+                stroke-width="0.2"
+                x="-0.38"
+                y="-0.38"
+                width="0.76"
+                height="0.76"
+                rx="0.15"
+              />
+            </g>
+          {/if}
+
+          {#each nums as row, i}
+            <g transform="translate(0 {i})">
+              {#each row as { n, locked }, j}
+                <g transform="translate({j} 0)">
                   <g
-                    on:click|stopPropagation={() => handleFocus({ c: j, r: i })}
-                    style:cursor="pointer"
+                    class:solved
+                    style="animation-delay: {(i + j) % 2 ? 0 : 0.1}s"
                     opacity="0"
                   >
                     <rect
+                      fill="none"
+                      stroke="#e98e31"
+                      stroke-width="0.2"
                       x="-0.38"
                       y="-0.38"
                       width="0.76"
@@ -242,14 +270,53 @@
                       rx="0.15"
                     />
                   </g>
-                {/if}
-              </g>
-            {/each}
-          </g>
-        {/each}
+
+                  <rect
+                    fill={locked ? "#f17b23" : "#fafafa"}
+                    x="-0.38"
+                    y="-0.38"
+                    width="0.76"
+                    height="0.76"
+                    rx="0.15"
+                  />
+                  {#if n !== 0}
+                    <text
+                      text-anchor="middle"
+                      dominant-baseline="middle"
+                      y="0.03"
+                      font-size="0.4"
+                      font-weight="700"
+                      font-family="monospace"
+                      fill={issues.includes(i * puzzle.size + j)
+                        ? "#e98e31"
+                        : "#1a1a1a"}>{n}</text
+                    >
+                  {/if}
+                  {#if !locked && !solved}
+                    <g
+                      on:click|stopPropagation={() =>
+                        handleFocus({ c: j, r: i })}
+                      style:cursor="pointer"
+                      opacity="0"
+                    >
+                      <rect
+                        x="-0.38"
+                        y="-0.38"
+                        width="0.76"
+                        height="0.76"
+                        rx="0.15"
+                      />
+                    </g>
+                  {/if}
+                </g>
+              {/each}
+            </g>
+          {/each}
+        </g>
       </g>
     </g>
   </svg>
+
   <div>
     {#each buttons as n}
       <button
@@ -270,8 +337,8 @@
   }
 
   :global(body) {
-    color: hsl(0, 0%, 97%);
-    background: hsl(249, 13%, 12%);
+    color: #fafafa;
+    background: #1a1a1a;
     min-height: 100vh;
     display: grid;
     place-items: center;
@@ -279,8 +346,8 @@
 
   article {
     margin: 1rem auto;
-    width: 100%;
-    max-width: 24rem;
+    max-width: 28rem;
+    width: 100vmin;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -311,23 +378,24 @@
   }
 
   div::-webkit-scrollbar-track {
-    background: hsla(0, 0%, 97%, 0.4);
+    background: #fafafa55;
   }
 
   div::-webkit-scrollbar-thumb {
-    background: hsl(0, 0%, 97%);
+    background: #fafafa;
   }
 
   button {
     width: 2.5rem;
     height: 2.5rem;
-    color: hsl(249, 13%, 12%);
-    background: hsl(0, 0%, 97%);
+    color: #1a1a1a;
+    background: #fafafa;
     border-radius: 0.5rem;
     border: none;
     font-family: monospace;
     display: block;
     font-size: 1.25rem;
+    margin: 0;
   }
 
   .solved {
