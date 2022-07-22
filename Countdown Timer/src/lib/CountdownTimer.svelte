9a0e6f837c39b8cdc292d60898869ff3357f1ca2
borntofrappe <borntofrappe@gmail.com>
2022-07-22 21:05:55 +0200

    feat: :sparkles: add Timer.svelte
    add component to complete the second part of the countdown timer and count down to zero

--- Countdown Timer/src/lib/CountdownTimer.svelte
+++ Countdown Timer/src/lib/CountdownTimer.svelte
@@ -1,7 +1,10 @@
 <script>
   import Countdown from "./Countdown.svelte";
+  import Timer from "./Timer.svelte";
 
   export let countdown = 5;
+  export let timer = 20;
 </script>
 
-<Countdown time={countdown} />
+<!-- <Countdown time={countdown} /> -->
+<Timer time={timer} />
---
+++ Countdown Timer/src/lib/Timer.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let time = 20;
+
+  onMount(() => {
+    let date = new Date(new Date().getTime() + time * 1000);
+
+    const interval = setInterval(() => {
+      if (time === 0) {
+        clearInterval(interval);
+      } else {
+        time = Math.round((date.getTime() - new Date().getTime()) / 1000);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+</script>
+
+<svg viewBox="-15.5 -15.5 30 30">
+  <g
+    text-anchor="middle"
+    dominant-baseline="central"
+    font-family="sans-serif"
+    paint-order="stroke"
+    fill="#ffffff"
+    stroke="#3c3c3c"
+    stroke-width="1.65"
+    stroke-linejoin="round"
+    stroke-linecap="round"
+    font-size="15"
+  >
+    <text>{time}</text>
+  </g>
+</svg>
+
+<style>
+  svg {
+    outline: 1px solid;
+    max-width: 10rem;
+  }
+</style>
