b6f10ab0e26200dc05d263b74eab7de864ad30e7
borntofrappe <borntofrappe@gmail.com>
2022-07-25 14:04:09 +0200

    feat: :sparkles: flip card with transform properties
    create a card with a single button, show a default emoji for the front, show the card's emoji for the back and through a ::before pseudo element

--- Match in Pairs/README.md
+++ Match in Pairs/README.md
@@ -1,3 +1,28 @@
 # Match in Pairs
 
 Match cards in pairs, much similarly to the game proposed by Netlify as you wait for the deploy to finish.
+
+## Flipped
+
+Markup.
+
+```html
+<button class="flipped" data-value="🧡">
+  <!-- front -->
+  🃏
+</button>
+```
+
+Style.
+
+```css
+button.flipped {
+  transform: rotateY(180deg);
+}
+
+button::before {
+  content: attr(data-value);
+}
+```
+
+Quite interestingly, the button receives click events with the class of `.flipped`, but on Firefox and not on Chrome. Since it would be possible to only flip the card in one sense, and the script should handle a possible flip back, the issue does not create a huge problem.
--- Match in Pairs/src/lib/MatchInPairs.svelte
+++ Match in Pairs/src/lib/MatchInPairs.svelte
@@ -1,1 +1,67 @@
-<h1>MatchInPairs</h1>
+<script>
+  const options = ["👋🏻", "🧡", "✨", "🌍", "🐦", "✏️", "🔥", "🙌"];
+
+  const cards = options.map((value) => ({
+    value,
+    flipped: false,
+  }));
+
+  const flipCard = (i) => {
+    cards[i].flipped = !cards[i].flipped;
+  };
+</script>
+
+<ul>
+  {#each cards as { value, flipped }, i}
+    <li>
+      <button class:flipped on:click={() => flipCard(i)} data-value={value}>
+        🃏
+      </button>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  li {
+    display: inline-block;
+    perspective: 200px;
+  }
+
+  button {
+    border: none;
+    background: none;
+    margin: 0;
+    padding: 0;
+    display: block;
+    aspect-ratio: 1/1;
+    transform: rotateY(0deg);
+    transform-style: preserve-3d;
+    position: relative;
+    transition: transform 0.5s ease-in-out;
+    z-index: 0;
+  }
+
+  button,
+  button::before {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2.5rem;
+    backface-visibility: hidden;
+  }
+
+  button::before {
+    content: attr(data-value);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform: rotateY(180deg);
+    z-index: -1;
+  }
+
+  button.flipped {
+    transform: rotateY(180deg);
+  }
+</style>
