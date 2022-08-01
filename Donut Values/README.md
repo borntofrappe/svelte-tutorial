3e237119ada9bd1415d98858c0083f0e534936a6
borntofrappe <borntofrappe@gmail.com>
2022-08-01 22:15:55 +0200

    feat: :sparkles: create custom donut with svg syntax
    create the visual for the custom donut chart without d3 and only svg syntax

--- Donut Values/README.md
+++ Donut Values/README.md
@@ -1,3 +1,25 @@
 # Donut Values
 
 Highlight a specific value and an average in a donut chart.
+
+## SVG donut
+
+Playing around with SVG syntax it is actually possible to create the donut chart without a third library like `d3`. The main trick relates to the `pathLength` attribute set on the `<path>` element which creates the outline for the donut.
+
+```svelte
+<path {pathLength} />
+```
+
+`stroke-dasharray` and `stroke-dashoffset` are relative to the length described by the attribute.
+
+Set `pathLength` to `100` as in 100 percent. Set `stroke-dasharray` to the same amount and then `stroke-dashoffset` to a value corresponding to the input value. Technically `100 - value` since a value of `100` completely hides the path, a value of `0` shows the whole piece.
+
+For the label devoted to the average define a larger path to position the text with `<textPath>`. It would be possible to position the label with `startOffset`, but ultimately I decided to compute an angle and rotate the label from its center.
+
+```svelte
+<g transform="rotate({angle})">
+  <!-- text & textPath -->
+</g>
+```
+
+The angle also comes in handy to position a `<mask>` element for a custom indentation carving the donut at the coordinate of the mean.
--- Donut Values/src/lib/Donut.svelte
+++ Donut Values/src/lib/Donut.svelte
@@ -1,1 +1,70 @@
-<h1>Hello donut</h1>
+<script>
+  export let value = 57;
+  export let average = 83;
+
+  const pathLength = 100;
+  $: offset = pathLength - value;
+  $: angle = (360 * average) / pathLength;
+</script>
+
+<svg viewBox="-70 -70 140 140">
+  <defs>
+    <path id="tp" d="M 0 -60 a 60 60 0 0 1 0 120 60 60 0 0 1 0 -120" />
+    <path
+      {pathLength}
+      fill="none"
+      stroke-width="10"
+      stroke-linecap="round"
+      id="p"
+      d="M 0 -50 a 50 50 0 0 1 0 100 50 50 0 0 1 0 -100"
+    />
+
+    <mask id="m">
+      <rect fill="white" x="-70" y="-70" width="140" height="140" />
+
+      <g transform="rotate({angle})">
+        <g transform="translate(0 -55)">
+          <path d="M -5 0 h 10 l -5 5z" />
+        </g>
+      </g>
+    </mask>
+  </defs>
+  <g mask="url(#m)">
+    <use href="#p" stroke="hsl(0, 0%, 90%)" />
+    <use
+      href="#p"
+      stroke="currentColor"
+      stroke-dasharray={pathLength}
+      stroke-dashoffset={offset}
+    />
+  </g>
+  <g
+    text-anchor="middle"
+    dominant-baseline="central"
+    font-size="20"
+    font-weight="700"
+    fill="currentColor"
+  >
+    <text>
+      {Math.floor(pathLength - offset)}%
+    </text>
+  </g>
+
+  <g font-size="10" style:text-transform="uppercase" fill="hsl(0, 0%, 60%)">
+    <g transform="rotate({angle})">
+      <text>
+        <textPath href="#tp">
+          {average}% Average
+        </textPath>
+      </text>
+    </g>
+  </g>
+</svg>
+
+<style>
+  svg {
+    color: hsl(125, 50%, 60%);
+    display: block;
+    max-width: 20rem;
+  }
+</style>
